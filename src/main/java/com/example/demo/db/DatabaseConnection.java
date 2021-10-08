8bf12468e087f6862416d2f2fbf905462d21bfbe
peranovicc <cvijanperanovic@aol.com>
2021-10-08 19:39:15 +0200

    Database connection

--- src/main/java/com/example/demo/db/DatabaseConnection.java
+++ src/main/java/com/example/demo/db/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
     private static Connection conn = null;
 
     static{
-        String url = "jdbc:postgresql://localhost:5432/fridgeapp?user=postgres&password=root";
+        String url = System.getenv("JDBC_DATABASE_URL");
         try {
             conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
